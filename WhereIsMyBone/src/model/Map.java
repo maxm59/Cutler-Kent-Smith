692b81beb4f361cf571e90025a7fa148a7570c85
Richard Smith <rictersmith@hotmail.com>
2016-10-04 22:09:40 -0700

    10.4.15 - Map class built.

---
+++ WhereIsMyBone/src/model/Map.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+//
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ricte
+ */
+public class Map implements Serializable{
+        
+    //class instance variables
+    private Integer rowCount;
+    private Integer columnCount;
+
+    public Map() {
+    }
+    
+    
+
+    public Integer getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(Integer rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public Integer getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(Integer columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.rowCount);
+        hash = 29 * hash + Objects.hashCode(this.columnCount);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (!Objects.equals(this.rowCount, other.rowCount)) {
+            return false;
+        }
+        if (!Objects.equals(this.columnCount, other.columnCount)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
--- WhereIsMyBone/src/whereismybone/WhereIsMyBone.java
+++ WhereIsMyBone/src/whereismybone/WhereIsMyBone.java
@@ -5,6 +5,8 @@
  */
 package whereismybone;
 
+import model.Map;
+
 /**
  *
  * @author ricte
@@ -16,6 +18,16 @@ public class WhereIsMyBone {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Map related
+        Map map = new Map();
+        map.setColumnCount(5);
+        map.setRowCount(5);
+                
+        //Test Map related items
+        String mapInfo = map.toString();
+        System.out.println(mapInfo);
+        
     }
     
 }
