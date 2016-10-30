9d660f92ef903554d3af7b583af5c5b4234cf738
Smith-Rick <rictersmith@hotmail.com>
2016-10-29 20:23:15 -0700

    10.23.16 - Updated Help Menu with everything

--- WhereIsMyBone/src/byui/cit260/whereismybone/view/HelpMenuView.java
+++ WhereIsMyBone/src/byui/cit260/whereismybone/view/HelpMenuView.java
@@ -32,7 +32,7 @@ public class HelpMenuView {
                 "\n         H =   How to play the game.              " +
                 "\n         M =   How to move around.                " +
                 "\n         C =   How to use Clues.                  " +
-                "\n         V =   Villans Details                    " +
+                "\n         V =   Villains Details                    " +
                 "\n         X =   Exit Help Menu.                    " +
                 "\n                                                  " +
                 "\n         To begin, please enter your choice:      " +
@@ -113,18 +113,107 @@ public class HelpMenuView {
     }
     private void displayObjectGameMenu(){
         System.out.println("Display Game Objective");
+               String details = "";
+        details = 
+                  "==================================================" +
+                "\n== HELP: Game Objectives =========================" +
+                "\n==================================================" +
+                "\n=                                                =" +
+                "\n=  Your favorite bone has been stolen and you    =" +
+                "\n=  suspect it is one of the members in the Cat   =" +
+                "\n=  DeVil Gang, but who took it and where is it?  =" +
+                "\n=                                                =" +
+                "\n=  Your mission is to find bone and aprehend the =" +
+                "\n=  theif. You only have 24 hours to do so or     =" +
+                "\n=  they will have gotten away free and clear.    =" +
+                "\n=                                                =" +
+                "\n=  Search for clues, pick up items that might    =" +
+                "\n=  help you along the way. Interview suspects    =" +
+                "\n=  and witnesses.                                =" +
+                "\n=                                                =" +
+                "\n=  Avoid Duh Catcher. If you encounter him, you  =" +
+                "\n=  will be forced back to your previous location.=" +
+                "\n=                                                =" +
+                "\n=  If time runs out, you will lose the game.     =" +
+                "\n=                                                =" +
+                "\n==================================================" ;
+
+
+        System.out.println(details);
     }
     
     private void displayHowToMenu(){
         System.out.println("Display How TO Play Details");
+               String details = "";
+        details = 
+                  "==================================================" +
+                "\n== HELP: How to Play =============================" +
+                "\n==================================================" +
+                "\n=                                                =" +
+                "\n= EVALUATING IF THIS IS NEEDED. SEEMS REDUNDANT  =" +
+                "\n= TO THE OBJECTIVE MENU.                         =" +
+                "\n=                                                =" +
+                "\n=                                                =" +
+                "\n=                                                =" +
+                "\n==================================================" ;
+
+
+        System.out.println(details);
     }
     
     private void displayHowToMove(){
         System.out.println("Display info on how to move");
+               String details = "";
+        details = 
+                  "==================================================" +
+                "\n== HELP: How To Move =============================" +
+                "\n==================================================" +
+                "\n=                                                =" +
+                "\n= When you are ready to move, bring up the MAP   =" +
+                "\n= menu. You will be given a list of options.     =" +
+                "\n= Select the 'Move' option and enter a 2 digit   =" +
+                "\n= code to move.                                  =" +
+                "\n=                                                =" +
+                "\n= ___________________________________________   =" +
+                "\n=    |   1   |   2   |   3   |   4   |   5   |   =" +
+                "\n= ___|-------|-------|-------|-------|-------|   =" +
+                "\n=    | Park  |Vacant |Police |Animal | Pound |   =" +
+                "\n=  1 |       | House | Office| Care  |       |   =" +
+                "\n= ___|_______|_______|_______|_______|_______|   =" +
+                "\n=    |Bakery |Food   |Drive  |Da Lake|Cat    |   =" +
+                "\n=  2 |       | Place | Inn   |       | Alley |   =" +
+                "\n= ___|_______|_______|_______|_______|_______|   =" +
+                "\n=    |Care   |Smith  |  YOUR |DeVil  |Grocery|   =" +
+                "\n=  3 | Home  | House |  HOME | House | Store |   =" +
+                "\n= ___|_______|_______|_______|_______|_______|   =" +
+                "\n=    |Fish-  |Zoo    |Zoo    |Zoo    |Zoo    |   =" +
+                "\n=  4 | monger|       |       |       |       |   =" +
+                "\n= ___|_______|_______|_______|_______|_______|   =" +
+                "\n=    |School |School |School |School |Zoo    |   =" +
+                "\n=  5 | Front | Cafe  |Playgrd|Parking|       |   =" +
+                "\n= ___|_______|_______|_______|_______|_______|   =" +
+                "\n=                                                =" +
+                "\n==================================================" ;
+
+
+        System.out.println(details);
     }
     
     private void displayClues(){
-        System.out.println("Display Clue info");
+        System.out.println("Display Clue info");       
+        String details = "";
+        details = 
+                  "==================================================" +
+                "\n== HELP: Clues ===================================" +
+                "\n==================================================" +
+                "\n=                                                =" +
+                "\n= Clues help you identify where to move next, or =" +
+                "\n= who the criminal might be. Check the clue log. =" +
+                "\n=                                                =" +
+                "\n==================================================" ;
+
+
+        System.out.println(details);
     }
     private void displayVillans(){
         System.out.println("Display Villan info");
@@ -132,7 +221,7 @@ public class HelpMenuView {
         String details = "";
         details = 
                   "==================================================" +
-                "\n== HELP: Villan Details ==========================" +
+                "\n== HELP: Villain Details =========================" +
                 "\n==================================================" +
                 "\n= Mrs. Cat DeVil                                 =" +
                 "\n=  Mrs. DeVil is the leader of the Cat DeVil     =" +
@@ -155,6 +244,15 @@ public class HelpMenuView {
                 "\n=  scar across his right eye. His one good blue  =" +
                 "\n=  eye will pierce you to the bone. He is the    =" +
                 "\n=  blackest of the alley cats.                   =" +
+                "\n=                                                =" +
+                "\n= Tom                                            =" +
+                "\n=  Your average tom cat.                         =" +
+                "\n=                                                =" +
+                "\n= Kit                                            =" +
+                "\n=  This guy is a ball of fuzz.                   =" +
+                "\n=                                                =" +
+                "\n= Rex                                            =" +
+                "\n=  You know Rex. T-Rex.                          =" +
                 "\n=                                                =" +
                 "\n==================================================" ;
 
