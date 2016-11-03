1954206174bc1d8de84838993703bb936c1bf89c
Smith-Rick <rictersmith@hotmail.com>
2016-11-02 21:58:32 -0700

    10.23.16 - Updated Help Menu with everything

---
+++ WhereIsMyBone/src/byui/cit260/whereismybone/view/ComplexCalculationView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.whereismybone.view;
+
+/**
+ *
+ * @author Smith-Rick
+ * 
+ * This view class contains the views for the complex calculations performed
+ * during the game. 
+ * 
+ * There are 4 main displays groups. 
+ * 1) Square foot area of the yard - Team
+ * 2) Volume of the cylinder - Brian? 
+ * 3) Cubic Inches - Maxi?
+ * 4) Energy Conversion - Rick
+ */
+public class ComplexCalculationView {
+    
+}
