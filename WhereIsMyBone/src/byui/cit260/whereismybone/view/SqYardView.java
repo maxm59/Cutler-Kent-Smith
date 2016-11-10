9d6710f4dcdabfbd7ed0475dcee9cde8c1125092
Smith-Rick <rictersmith@hotmail.com>
2016-11-10 00:08:09 -0800

    11.09.16 - Updates for SqYrdview

--- WhereIsMyBone/src/byui/cit260/whereismybone/view/SqYardView.java
+++ WhereIsMyBone/src/byui/cit260/whereismybone/view/SqYardView.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
  * the square area calculation of the backyard.
  * 
  */
-public class SqYardView {
-    
-    private String menuOptions = "";
+public class SqYardView extends View{
+//    
+//    private String menuOptions = "";
     
     public SqYardView(){
         
         //this.displayMenu();
         
-        this.menuOptions =
+        super(
                   "***************************************************" +
                 "\n*                                                 *" +
                 "\n*     This is my backyard. My bone has to be      *" +
@@ -28,52 +28,52 @@ public class SqYardView {
                 "\n*     Please enter a number for the width:        *" +
                 "\n*     Please enter a number for the length:       *" +
                 "\n*                                                 *" +
-                "\n*     The total                                             *" +
-                "\n*==================================================" ;           
+                "\n*     The total                                   *" +
+                "\n*==================================================" );           
         
     }
     
-    private void displayMenu() {
-        
-        menuOptions = getMenuOptions();
-        
-    }
+//    private void displayMenu() {
+//        
+//        menuOptions = getMenuOptions();
+//        
+//    }
     
-    private String getMenuOptions() {
-        
-        boolean exitMenu = false;
-        String menu = "";
-        
-        Scanner keyboard = new Scanner(System.in);
-        
-        while (!exitMenu){
-            System.out.println(menuOptions);
-            String input = keyboard.nextLine();
-
-            if(input.length()  >= 1)
-            {
-                
-                char keyEntered = input.toUpperCase().charAt(0);
-                
-                if(keyEntered == 'X'){
-                    //User wants to exit
-                    menu = "X";
-                    exitMenu = true;
-                }
-                else{
-                    
-                    exitMenu = doAction(input);
-                }
-                
-            }
-            else{
-             System.out.println("Your choice must be at least 1 character.");
-            }
-        }   
-        return menu;               
-    }
+//    private String getMenuOptions() {
+//        
+//        boolean exitMenu = false;
+//        String menu = "";
+//        
+//        Scanner keyboard = new Scanner(System.in);
+//        
+//        while (!exitMenu){
+//            System.out.println(menuOptions);
+//            String input = keyboard.nextLine();
+//
+//            if(input.length()  >= 1)
+//            {
+//                
+//                char keyEntered = input.toUpperCase().charAt(0);
+//                
+//                if(keyEntered == 'X'){
+//                    //User wants to exit
+//                    menu = "X";
+//                    exitMenu = true;
+//                }
+//                else{
+//                    
+//                    exitMenu = doAction(input);
+//                }
+//                
+//            }
+//            else{
+//             System.out.println("Your choice must be at least 1 character.");
+//            }
+//        }   
+//        return menu;               
+//    }
     
-    private boolean doAction(String choice){
+    public boolean doAction(String choice){
         
         choice = choice.toUpperCase();
         
@@ -92,21 +92,21 @@ public class SqYardView {
         return false;  
     
     }
-    public void displayComplexCalculationView() {
-       
-        boolean done = false; //set the flag to not done
-        do{
-            //prompt for menu option
-            String menuOption = this.getMenuOptions();
-            
-            if (menuOption.toUpperCase().equals("X"))//user wants to quit
-                return; //exit the game
-            
-            //do the requested action and display the next view
-            done = this.doAction(menuOption);
-            
-        }while (!done);  
-    }
+//    public void displayComplexCalculationView() {
+//       
+//        boolean done = false; //set the flag to not done
+//        do{
+//            //prompt for menu option
+//            String menuOption = this.getMenuOptions();
+//            
+//            if (menuOption.toUpperCase().equals("X"))//user wants to quit
+//                return; //exit the game
+//            
+//            //do the requested action and display the next view
+//            done = this.doAction(menuOption);
+//            
+//        }while (!done);  
+//    }
     
     public void displayWidthOfTheYard(){
         System.out.println("Debug: Display Cubic Inches View");
