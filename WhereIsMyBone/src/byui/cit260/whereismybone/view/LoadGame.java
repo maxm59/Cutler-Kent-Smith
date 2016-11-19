450f34a7117167de3ef5e62f755007e0fd40ab53
Smith-Rick <rictersmith@hotmail.com>
2016-11-18 16:48:48 -0800

    11.18.16 - GameWon, SaveGame and LoadGame views.

---
+++ WhereIsMyBone/src/byui/cit260/whereismybone/view/LoadGame.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.whereismybone.view;
+
+import byui.cit260.whereismybone.control.ComplexCalculationsControl;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Smith-Rick
+ */
+public class LoadGame extends View{
+    
+    public LoadGame(){
+         super(
+                  "==================================================" +
+                "\n               WHERE IS MY BONE?                  " +
+                "\n==================================================" +
+                "\n                                                  " +
+                "\n  LOAD GAME:                                      " +
+                "\n                                                  " +
+                "\n    A) EMPTY" +
+                "\n    B) EMPTY" +
+                "\n    C) EMPTY" +
+                "\n                                                  " +         
+                "\n==================================================" +
+                "\n  Please enter your choice: ____                  " +
+                "\n=================================================="  ); 
+        
+    }
+
+    @Override
+    public boolean doAction(String choice) {
+           
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "A": 
+                System.out.println("\n*** Loading from Space A ***");
+                break;
+            case "B":
+                System.out.println("\n*** Loading from Space B ***");
+                break;
+            case "C":
+                System.out.println("\n*** Loading from Space C ***");
+                break;
+            default:
+                System.out.println("\n***Invalid Selection*** Try again");
+                break; 
+                
+        }
+        return false;  
+    }
+    
+}
---
+++ WhereIsMyBone/src/byui/cit260/whereismybone/view/SaveGame.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.whereismybone.view;
+
+import byui.cit260.whereismybone.control.ComplexCalculationsControl;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Smith-Rick
+ */
+public class SaveGame extends View{
+    
+    public SaveGame(){
+         super(
+                  "==================================================" +
+                "\n               WHERE IS MY BONE?                  " +
+                "\n==================================================" +
+                "\n                                                  " +
+                "\n  SAVE GAME:                                      " +
+                "\n                                                  " +
+                "\n    A) EMPTY" +
+                "\n    B) EMPTY" +
+                "\n    C) EMPTY" +
+                "\n                                                  " +         
+                "\n==================================================" +
+                "\n  Please enter your choice: ____                  " +
+                "\n=================================================="  ); 
+        
+    }
+
+    @Override
+    public boolean doAction(String choice) {
+           
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "A": 
+                System.out.println("\n*** Saving to Space A ***");
+                break;
+            case "B":
+                System.out.println("\n*** Saving to Space B ***");
+                break;
+            case "C":
+                System.out.println("\n*** Saving to Space C ***");
+                break;
+            default:
+                System.out.println("\n***Invalid Selection*** Try again");
+                break; 
+                
+        }
+        return false;  
+    }
+    
+}
