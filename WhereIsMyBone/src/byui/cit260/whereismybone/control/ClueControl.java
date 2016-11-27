affa8b10eadfe919db99c35b02106f9ee3f4f016
Brian <Brian@BriansSony>
2016-11-26 21:31:15 -0800

    11/26/2016 Brian ClueControl added (work in progress)

--- WhereIsMyBone/src/byui/cit260/whereismybone/control/ClueControl.java
+++ WhereIsMyBone/src/byui/cit260/whereismybone/control/ClueControl.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package byui.cit260.whereismybone.control;
-
+import byui.cit260.whereismybone.model.Clue;
+import byui.cit260.whereismybone.model.Game;
+import byui.cit260.whereismybone.model.GameTime;
+import byui.cit260.whereismybone.model.Map;
+import whereismybone.WhereIsMyBone;
 /**
  *
  * @author Brian Kent
@@ -12,10 +16,37 @@ package byui.cit260.whereismybone.control;
  */
 public class ClueControl {
     
+    public ClueControl(){
+                
+    }
     
+    public static void createNewClue (String description) {
+        
+        //clue.setClue(description);
+        
+        //Map map = new Map();
+        Map map = MapControl.createMap();
+        game.setMap(map);
+        
+        
+//        System.out.println("\n*** Create New Game *** " + map.getCurrentLocation().getName());
+        
+        game.getPlayer().setLocation(map.getCurrentLocation());  
+        
+//        System.out.println("\n*** Create New Game *** " + map.getCurrentLocation().getName());
+                
+        //GameTime gt = new GameTime();
+        GameTime gt = GameControl.createGameTime();
+        game.setGameTime(gt);
+
+        WhereIsMyBone.setCurrentGame(game);
+        
+        
+    }
     
-    
-    
+    public static void ClueLog (String description){
+        
+    }
     
     
 }
