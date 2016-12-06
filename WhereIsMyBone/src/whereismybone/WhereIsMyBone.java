85eae7161c3c6158c2adfbcbaea4438b496d993a
ricte <ricte@LivingRoomDell>
2016-12-05 20:13:18 -0800

    12.3.16 - L12 Individual Assignment - Print Map to File.

--- WhereIsMyBone/src/whereismybone/WhereIsMyBone.java
+++ WhereIsMyBone/src/whereismybone/WhereIsMyBone.java
@@ -8,8 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class WhereIsMyBone {
     
